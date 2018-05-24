22575a233bf2929f05dcf53a6bce9adf55059497
GabsReDeal <louisgabriel@ymail.com>
2018-05-25 01:27:31 +0200

    Update 10
    Web Application
    -- About Us page is not better, but not yet fully done
    -- Contact Us page now includes a form where the user enters his details, message and submits
    -- Added images for all IDs in the database

--- Webapp/about.php
+++ Webapp/about.php
@@ -7,7 +7,9 @@
 <div class="container">
     <div class="row">
         <div class="col-lg-12">
-            <h2>This is the about page</h2>
+            <h2>About Us</h2>
+            <p>This website allows you to check your personal information from the confort of your home!<p>
+            <p>You may also check about your licence and vehicles registered</p>
         </div>
     </div>
 </div>
--- Webapp/contact.php
+++ Webapp/contact.php
@@ -5,10 +5,46 @@
 ?>
 
 <div class="container">
-    <div class="row">
-        <div class="col-lg-12">
-            <h2>This is the contact page</h2>
-        </div>
+    <h2>Contact Us</h2>
+    <div class="col-lg-12">
+        <form method="post">
+            <div class="form-row mt-3">
+                <div class="form-group col-sm-6">
+                    <label for="inputfName">First name</label>
+                    <input type="text" class="form-control" name="fname" id="inputfName" placeholder="Enter your first name" required>
+                </div>
+                <div class="form-group col-sm-6">
+                    <label for="inputlName">Last Name</label>
+                    <input type="text" class="form-control" name="lname" id="inputlName" placeholder="Enter your last name" required>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="inputEmail">Email</label>
+                <input type="email" class="form-control" name="email" id="inputEmail" placeholder="Enter your email" required>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-sm-12">
+                    <label for="message">Message</label>
+                    <textarea class="form-control" id="message" name="message" rows="6" style="resize:none" placeholder="Any issues or suggetions..." required></textarea>
+                </div>
+            </div>
+            <button type="submit" name="submit" class="btn btn-block btn-primary">Submit</button>
+        </form>        
+    </div>
+    <div class="col-lg-12 mt-3">
+    <?php
+        if(isset($_POST['submit']))
+        {
+            if((empty($_POST['fname'])) || (empty($_POST['lname'])) || (empty($_POST['email'])) || (empty($_POST['message'])))
+            {
+                echo "<div class='alert alert-danger'>Please input all information!</div>";
+            }
+            else
+            {
+                echo "<div class='alert alert-success'>Your message has been submitted!</div>";
+            }
+        }
+    ?>
     </div>
 </div>
 
